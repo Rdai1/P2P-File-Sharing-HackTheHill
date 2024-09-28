ba5522e6059f33dff78118b02b21ec1f1985b104
Mahad T <mahad.toukaleh56@gmail.com>
2024-09-28 01:50:48 -0400

    Created basic Node Class structure with attributes and constructors

--- Node.java
+++ Node.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
+    // attributes
+    private String peerID;
+    private String IPAddress;
+    private int port;
+    private List<Node> peers; // this is the list of peers
+
+    // Constructor
+    public Node(String peerId, String ipAddress, int port, List<Node> peers) {
+        this.peerID = peerId;
+        this.IPAddress = ipAddress;
+        this.port = port;
+        this.peers = peers;
+    }
+
+    // This method will typically read from a file (filePath), then divide into chunks of 512 bytes.
+    public List<byte[]> chunkFile(String filePath) {
+        return new ArrayList<>(); // placeholder code to not get errors.
+    }
 }
