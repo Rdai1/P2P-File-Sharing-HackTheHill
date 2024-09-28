538a50af14a8d9b4347e153871ef5d7cdf7070b8
Raymond Dai <53146446+Rdai1@users.noreply.github.com>
2024-09-28 13:00:29 -0400

    Added File Handling
    - Added ChunkFile
    - Created FileChunk Class
    - Created File handling class

---
+++ FileHandling.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHandling {
+    private static final int CHUNK_SIZE = 512;
+    // This method will read from a file (filePath), then divide into chunks of 512 bytes.
+    public List<FileChunk> chunkFile(String filePath) {
+        
+        List<FileChunk> chunks = new ArrayList<>();
+
+        try(FileInputStream inputFile = new FileInputStream(filePath)){
+            byte[] buffer = new byte[CHUNK_SIZE];
+            int bytesRead;
+            
+            int chunkNumber = 0;
+            while ((bytesRead = inputFile.read(buffer)) != -1){
+                byte[] data = new byte[bytesRead];
+                System.arraycopy(buffer, 0, data, 0, bytesRead);
+                chunks.add(new FileChunk(chunkNumber++, data));
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return chunks;
+    }
+
+
+    // This method will reassemble the previously chunked data back into a complete file.
+    private void assembleFile(List<byte[]> chunks, String outputFilePath) throws IOException {
+
+    }
+}
